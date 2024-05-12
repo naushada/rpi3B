bf69710a535c0ec70e99bc252e69c72345701d2a
naushada <naushad.dln@gmail.com>
2024-05-12 13:21:49 +0530

    defining scope of const that same file

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -5,7 +5,7 @@
 
 #include "gpio.hpp"
 
-const std::unordered_map<GPIO::gpio_number, GPIO::pin_number> gpioNo2PinNo = {
+static const std::unordered_map<GPIO::gpio_number, GPIO::pin_number> gpioNo2PinNo = {
     {0,   3}, {1,   3}, {2,   3}, {3,   5}, {4,   7}, {5,  29}, {6,  31}, {7,  26}, {8,  24}, {9,  21},
     {10, 19}, {11, 23}, {12, 32}, {13, 33}, {14,  8}, {15, 10}, {16, 36}, {17, 11}, {18, 12}, {19, 35},
     {20, 38}, {21, 40}, {22, 15}, {23, 16}, {24, 18}, {25, 22}, {26, 37}, {27, 13}, {28,  3}, {29,  3},
