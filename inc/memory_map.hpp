37f7f99e7c440086fa064036ac9d3503527b8ef0
naushada <naushad.dln@gmail.com>
2024-05-13 14:33:27 +0530

    Updated

--- inc/memory_map.hpp
+++ inc/memory_map.hpp
@@ -106,9 +106,113 @@ struct GPIORegistersAddress {
 
 struct ClockRegistersAddress {
 
+    enum AddressOffset : std::uint32_t {
+        CM_GP0CTL,
+        CM_GP0DIV,
+        CM_GP1CTL,
+        CM_GP1DIV,
+        CM_GP2CTL,
+        CM_GP2DIV,
+        CM_GPn_MAX
+    };
+
+    enum ClockSource: std::uint32_t {
+        Ground = 0,
+        Oscillator = 1,
+        Testdebug0 = 2,
+        Testdebug1 = 3,
+        PLLA = 4,
+        PLLC = 5,
+        PLLD = 6,
+        HDMI_Auxiliary = 7
+        /**
+         * @brief
+                8-15 = GND
+                To avoid lock-ups and glitches do not change this
+                control while BUSY=1 and do not change this control
+                at the same time as asserting ENAB
+         */
+    };
+
+    enum ClockMASH : std::uint32_t {
+        Integer_division = 0,
+        Stage1_MASH = 1,
+        Stage2_MASH = 2,
+        Stage3_MASH = 3
+        /**
+         * @brief
+         *      To avoid lock-ups and glitches do not change this
+                control while BUSY=1 and do not change this control
+                at the same time as asserting ENAB.
+        */
+    };
+
+    using device_register = volatile std::atomic<std::uint32_t>; //@brief This ensures that this is a thread safe
+    ClockRegistersAddress() {}
+    ~ClockRegistersAddress() {}
+
+    /** 
+     * @brief Compiler give preference to this new operator over global new operator and invoke this new operator. 
+     * @param nBytes Number of bytes to be allocated
+     * @param region is the memory region to be return if present.
+     * @return pointer to void
+     */
+    void *operator new(std::size_t nBytes, void *region=nullptr) {
+        (void)nBytes;
+        if(nullptr == region) {
+            return reinterpret_cast<void *>((0x3F000000) + (0x00100000));
+        }
+        return(region);
+    }
+
+    /** 
+     * @brief
+     * address for m_register will be 
+     * [0x3F100000, 0x3F100004, 0x3F100008, 0x3F10000C, 0x3F100010, 0x3F100014, 0x3F100018, 0x3F10001C  ... 0x3F1000B0] 
+     **/
+    device_register m_register[AddressOffset::CM_GPn_MAX];
 };
 
-struct _InterruptRegisterAddress {
+struct InterruptRegisterAddress {
+    enum Register : std::uint32_t {
+        IRQ_Basic_Pending,
+        IRQ_Pending1,
+        IRQ_Pending2,
+        FIQ_Control,
+        Enable_IRQs_1,
+        Enable_IRQs_2,
+        Enable_Basic_IRQs,
+        Disable_IRQs_1,
+        Disable_IRQs_2,
+        Disable_Basic_IRQs,
+        IRQs_ALL_MAX
+    };
+
+    
+    using device_register = volatile std::atomic<std::uint32_t>; //@brief This ensures that this is a thread safe
+    InterruptRegisterAddress() {}
+    ~InterruptRegisterAddress() {}
+
+    /** 
+     * @brief Compiler give preference to this new operator over global new operator and invoke this new operator. 
+     * @param nBytes Number of bytes to be allocated
+     * @param region is the memory region to be return if present.
+     * @return pointer to void
+     */
+    void *operator new(std::size_t nBytes, void *region=nullptr) {
+        (void)nBytes;
+        if(nullptr == region) {
+            return reinterpret_cast<void *>(0x3F00B200);
+        }
+        return(region);
+    }
+
+    /** 
+     * @brief
+     * address for m_register will be 
+     * [0x3F00B200, 0x3F00B204, 0x3F00B208, 0x3F00B20C, 0x3F00B210, 0x3F00B214, 0x3F00B218, 0x3F00B21C  ... 0x3F00B220, 0x3F00B224] 
+     **/
+    device_register m_register[Register::IRQs_ALL_MAX];
 
 };
 
