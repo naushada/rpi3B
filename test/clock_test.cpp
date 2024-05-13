50688932fedf45cd44963bf2279fdf2b55086fe1
naushada <naushad.dln@gmail.com>
2024-05-13 22:19:00 +0530

    Updated

--- test/clock_test.cpp
+++ test/clock_test.cpp
@@ -76,7 +76,38 @@ TEST_F(CLOCKTest, ClockRegister_CM_GP2DIV_Read_BOTH) {
     EXPECT_EQ(res, 35);
 }
 
+TEST_F(CLOCKTest, ClockRegister_CM_GP0DIV_Write_BOTH) {
+    clock().write(RPi3B::ClockRegistersAddress::Register::CM_GP0DIV, RPi3B::ClockRegistersAddress::ClockDivisor::BOTH_VALUE, ((0xFFF << 12) | 0xFFF));
+    auto res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP0DIV, RPi3B::ClockRegistersAddress::ClockDivisor::BOTH_VALUE);
+    res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP0DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVF);
+    EXPECT_EQ(res, 0xFFF);
+    res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP0DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVI);
+    EXPECT_EQ(res, 0xFFF);
+}
 
+TEST_F(CLOCKTest, ClockRegister_CM_GP1DIV_Write_BOTH) {
+    clock().write(RPi3B::ClockRegistersAddress::Register::CM_GP1DIV, RPi3B::ClockRegistersAddress::ClockDivisor::BOTH_VALUE, ((0xFF2 << 12) | 0xFFD));
+    auto res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP1DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVF);
+    EXPECT_EQ(res, 0xFFD);
+    res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP1DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVI);
+    EXPECT_EQ(res, 0xFF2);
+}
+
+TEST_F(CLOCKTest, ClockRegister_CM_GP2DIV_Write_BOTH) {
+    clock().write(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::BOTH_VALUE, ((0x1 << 12) | 0x1));
+    auto res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVF);
+    EXPECT_EQ(res, 0x1);
+    res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVI);
+    EXPECT_EQ(res, 0x1);
+}
+
+TEST_F(CLOCKTest, ClockRegister_CM_GP2DIV_Write_BOTH_Var1) {
+    clock().write(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::BOTH_VALUE, ((21 << 12) | 854));
+    auto res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVF);
+    EXPECT_EQ(res, 854);
+    res = clock().read(RPi3B::ClockRegistersAddress::Register::CM_GP2DIV, RPi3B::ClockRegistersAddress::ClockDivisor::DIVI);
+    EXPECT_EQ(res, 21);
+}
 
 
 
